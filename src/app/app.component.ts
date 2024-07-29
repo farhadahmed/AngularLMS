0af65422debcb08c6883d38dce810c292e0711da
Farhad Ahmed <farhadahmed2007@gmail.com>
2024-07-29 15:30:10 -0700

    added standalone component home and connected app.component to use the new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, HomeComponent],
+  template:`
+    <main>
+      <header>
+      </header>
+      <section>
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
