e08af1f299b730e032d34034a3ccb269be0e0f5a
Farhad Ahmed <farhadahmed2007@gmail.com>
2024-07-29 17:24:55 -0700

    added CourseIcon component and connected it to the Home component

---
+++ src/app/course-icon/course-icon.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-icon',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      course-icon works!
+    </p>
+  `,
+  styleUrl: './course-icon.component.css'
+})
+export class CourseIconComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import { CourseIconComponent } from "../course-icon/course-icon.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CourseIconComponent],
   template: `
-    <p>
-      home works!
-    </p>
+    <section>
+        <form>
+          <input type="text" placeholder="Filter by city" #filter/>
+          <button class="primary" type="button">Search</button>
+        </form>
+    </section>
+    <section class="results">
+      <app-course-icon></app-course-icon>
+    </section>
   `,
   styleUrl: './home.component.css'
 })
