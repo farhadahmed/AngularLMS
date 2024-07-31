274268954c6b3adc0aad357367fe6de99cd8a323
Farhad Ahmed <farhadahmed2007@gmail.com>
2024-07-31 01:41:13 -0700

    add a link to details using routerLink

--- src/app/course-icon/course-icon.component.ts
+++ src/app/course-icon/course-icon.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CourseIcon } from '../course-icon';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-course-icon',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section>
       <img class="listing-photo" [src]="courseIcon.photo" alt="Exterior photo of {{courseIcon.name}}">
       <h2 class="listing-heading">{{ courseIcon.name }}</h2>
       <p class="listing-location">{{ courseIcon.city }}, {{ courseIcon.state }}</p>
+      <a routerLink="details">Learn More</a>
     </section>
   `,
   styleUrl: './course-icon.component.css'
