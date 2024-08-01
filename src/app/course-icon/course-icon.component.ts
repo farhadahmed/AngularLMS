3be7caf97ba835ac984c6cb3c77f7a87eae08133
Farhad Ahmed <farhadahmed2007@gmail.com>
2024-07-31 18:22:34 -0700

    pass courseIcon.id to route for details pages

--- src/app/course-icon/course-icon.component.ts
+++ src/app/course-icon/course-icon.component.ts
@@ -12,7 +12,7 @@ import { RouterModule } from '@angular/router';
       <img class="listing-photo" [src]="courseIcon.photo" alt="Exterior photo of {{courseIcon.name}}">
       <h2 class="listing-heading">{{ courseIcon.name }}</h2>
       <p class="listing-location">{{ courseIcon.city }}, {{ courseIcon.state }}</p>
-      <a routerLink="details">Learn More</a>
+      <a [routerLink]="['/details', courseIcon.id]">Learn More</a>
     </section>
   `,
   styleUrl: './course-icon.component.css'
